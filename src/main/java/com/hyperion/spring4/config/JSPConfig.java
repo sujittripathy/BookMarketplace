7194fa3fc4ecabdef69e34f9f405a9292228f205
sujittripathy <sujit.tripathy@icloud.com>
2016-09-25 23:19:38 -0700

    New files added (cherry picked from commit 6ed7d61)

---
+++ src/main/java/com/hyperion/spring4/config/JSPConfig.java
@@ -0,0 +1,36 @@
+package com.hyperion.spring4.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+/**
+ * Created by sujittripathy on 9/24/16.
+ */
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan("com.hyperion")
+public class JSPConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver internalResourceViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(2);
+        return resolver;
+    }
+
+}
